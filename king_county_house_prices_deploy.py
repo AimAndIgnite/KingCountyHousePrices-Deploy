813f9b6c69ec0f1683544ee61475938f88ba91d0
Andrian Leong <39163076+AimAndIgnite@users.noreply.github.com>
2022-12-30 17:13:44 +0700

    Update king_county_house_prices_deploy.py

--- king_county_house_prices_deploy.py
+++ king_county_house_prices_deploy.py
@@ -24,9 +24,10 @@ if st.checkbox('Show dataframe'):
     data
     
 xgb_model = xgb.XGBRegressor()
-# xgb_model.load_model("model_sf.json")
+xgb_model.load_model("model_sf.json")
+# xgb_model = pickle.load(open("finalized_model.pkl", "rb"))
 
-xgb_model = pickle.load(open("finalized_model.pkl", "rb"))
+
 
 st.subheader("Please select relevant features of your house:")
 
